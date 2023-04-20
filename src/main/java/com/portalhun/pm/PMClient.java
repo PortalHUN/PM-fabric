eb22ed026c1541792081208e4dc3d001506cfe0b
PortalHUN <giotomi03@gmail.com>
2023-04-20 15:03:09 +0200

    Initial commit

---
+++ src/main/java/com/portalhun/pm/PMClient.java
@@ -0,0 +1,10 @@
+package com.portalhun.pm;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PMClient implements ClientModInitializer {
+  @Override
+  public void onInitializeClient() {
+
+  }
+}
